7e05ce62a3ee96077667b5f8ce872feba9bb5e2d
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2022-12-10 12:05:01 +0300

    upd menu, unity version

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,10 +7,29 @@ namespace ARTEX.Rogue.Menus
 {
     public class Menu : MonoBehaviour
     {
+        [SerializeField] private string _gameSceneName = "";
+        [SerializeField] private GameObject _settingsPanel;
+        [SerializeField] private GameObject _aboutPanel;
         public void Play()
         {
-            LevelManager.LoadLevel("level1");
+            LevelManager.LoadLevel(_gameSceneName);
         }
+
+        public void OpenSettingsMenu()
+        {
+            _settingsPanel.SetActive(true);
+        }
+        public void OpenAboutPanel()
+        {
+            _aboutPanel.SetActive(true);
+        }
+
+        public void ClosePanels()
+        {
+            _settingsPanel.SetActive(false);
+            _aboutPanel.SetActive(false);
+        }
+
 
         public void Quit()
         {
